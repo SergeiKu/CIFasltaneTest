b82a40161301b2ead833abb54841121921c643dd
Sergey Kudryavtsev <sergey.developer@bk.ru>
2019-12-06 08:57:46 +0300

    tests added

---
+++ TestFastlaneProjectTests/TestFastlaneProjectTests.swift
@@ -0,0 +1,26 @@
+//
+//  TestFastlaneProjectTests.swift
+//  TestFastlaneProjectTests
+//
+//  Created by Sergey Kudryavtsev on 06.12.2019.
+//  Copyright © 2019 TestFastlane. All rights reserved.
+//
+
+import XCTest
+
+class TestFastlaneProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
